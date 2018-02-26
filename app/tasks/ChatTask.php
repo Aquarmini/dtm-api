07a44eed25d0cdc9c31d0af16f4e78a515c42864
李铭昕 <715557344@qq.com>
2018-02-26 18:47:20 +0800

    增加websocket脚本

--- app/tasks/ChatTask.php
+++ app/tasks/ChatTask.php
@@ -3,14 +3,58 @@
 namespace App\Tasks;
 
 use App\Core\Cli\Task\WebSocket;
+use swoole_http_request;
+use swoole_websocket_frame;
+use swoole_websocket_server;
+use Xin\Phalcon\Cli\Traits\Input;
 
-class ChatTask extends Task
+class ChatTask extends WebSocket
 {
+    use Input;
 
-    public function mainAction()
+    public function onConstruct()
     {
-
+        $this->port = 12000;
+        $this->host = '127.0.0.1';
     }
 
+    protected function beforeServerStart(swoole_websocket_server $server)
+    {
+        parent::beforeServerStart($server);
+        $config = $this->getConfig();
+        if ($this->option('daemonize')) {
+            $config['daemonize'] = true;
+        }
+        // 重置参数
+        $server->set($config);
+    }
+
+    public function connect(swoole_websocket_server $server, swoole_http_request $request)
+    {
+        // TODO: Implement connect() method.
+    }
+
+    public function message(swoole_websocket_server $server, swoole_websocket_frame $frame)
+    {
+        $fd = $frame->fd;
+        $data = $frame->data;
+        echo $fd . ':' . $data . PHP_EOL;
+        $server->push($fd, 'ss' . $data);
+    }
+
+    public function close(swoole_websocket_server $ser, $fd)
+    {
+        // TODO: Implement close() method.
+    }
+
+    protected function getConfig()
+    {
+        $pidsDir = di('config')->application->pidsDir;
+        return [
+            'pid_file' => $pidsDir . 'socket.pid',
+            'daemonize' => false,
+            'max_request' => 500,
+        ];
+    }
 }
 

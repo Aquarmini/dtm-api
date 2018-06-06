42fafd5b291a790f18df9a2a0ddd5099a1d08f16
李铭昕 <715557344@qq.com>
2018-06-06 18:21:41 +0800

    增加从Docker Jenkins发布到宿主机的发布脚本

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+ssh -i /var/jenkins_home/.ssh/id_rsa -tt root@172.17.0.1 "cd /www/limx/dtm-api && \
+git pull && \
+composer update --no-dev --prefer-dist -o && \
+service dtm.chat restart"

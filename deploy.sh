92ae2b06c9221c24f7e08d68d66a72662d43edcf
李铭昕 <715557344@qq.com>
2018-06-06 18:55:54 +0800

    增加echo jenkins 参数测试

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 ssh -i /var/jenkins_home/.ssh/id_rsa -tt root@172.17.0.1 "cd /www/limx/dtm-api && \
 git pull && \
 composer update --no-dev --prefer-dist -o && \
-service dtm.chat restart"
+service dtm.chat restart && echo $ip"
